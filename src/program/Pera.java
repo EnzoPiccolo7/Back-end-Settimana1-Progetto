671f35ff0cc66cb1ac47e68f42a7abc9359be0a9
enzo <enzo@enzo-ThinkPad-T470s-W10DG>
2022-07-22 18:30:51 +0200

    primo commit

---
+++ src/program/Pera.java
@@ -0,0 +1,15 @@
+package program;
+
+public class Pera implements Comparable<Pera>{
+    private int grammi;
+    private String tipo;
+
+    public Pera(String tipo, int grammi){
+        this.tipo = tipo;
+        this.grammi = grammi;
+    }
+
+    public int compareTo(Pera p){
+        return grammi - p.grammi;
+    }
+}
---
+++ src/program/TestMelaPera.java
@@ -0,0 +1,11 @@
+package program;
+
+public class TestMelaPera {
+
+	public static void main(String[] args) {
+		Mela mela = new Mela("golden",135);
+		Pera pera = new Pera("williamn",120);
+		
+	}
+
+}
